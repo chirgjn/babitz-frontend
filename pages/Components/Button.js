62bad0b85778ee0afc0abcdeffcd91fed8cecbf6
siddanand <chandiboy18@gmail.com>
2021-07-16 04:30:29 +0530

    Landing Page
    - Designed the landing Page using styled components and bootstrap

--- pages/Components/Button.js
+++ pages/Components/Button.js
@@ -1,25 +1,24 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Button = styled.button`
-width: 250px;
-background: #FFC535;
-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-border:none;
-border-radius: 25px;
-font-family: Oswald;
-font-style: normal;
-font-weight: normal;
-font-size: 20px;
-align-items: center;
-text-align: center;
-letter-spacing: -0.015em;
-padding:7px;
-@media (max-width: 768px) {
-  font-size: 15px;
-  width: 150px;
-  padding:5px;
-
-}
+  width: 250px;
+  background: #ffc535;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border: none;
+  border-radius: 25px;
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  align-items: center;
+  text-align: center;
+  letter-spacing: -0.015em;
+  padding: 7px;
+  @media (max-width: 768px) {
+    font-size: 15px;
+    width: 150px;
+    padding: 5px;
+  }
 `;
 
 export default Button;
