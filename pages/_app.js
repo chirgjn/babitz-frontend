3252f87801160e4100fbe3629cf4a116af5ad55c
siddanand <chandiboy18@gmail.com>
2021-07-20 03:41:44 +0530

    Edit Template One
    - Designed the edit template page for the first template

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,24 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      />
-      <link
-        href="https://fonts.googleapis.com/css?family=Oswald"
-        rel="stylesheet"
-      />
-      <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"
-      />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Oswald"
+          rel="stylesheet"
+        />
+        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"
+        />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
