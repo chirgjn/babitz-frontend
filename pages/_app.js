a453afe8def5da4ce6bd3f68c5df7cf1ebc3a65d
siddanand <chandiboy18@gmail.com>
2021-07-29 02:26:04 +0530

    Adding links for fonts and css

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <div>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Oswald"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"
+        />
+        <Component {...pageProps} />
+      </div>
+    );
+
 }
 
 export default MyApp
