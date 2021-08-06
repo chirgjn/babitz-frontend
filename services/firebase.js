5e0fc3056e9883f54b13b0dc6c424ec0a4ee2aa6
siddanand <chandiboy18@gmail.com>
2021-08-06 13:50:24 +0530

    Authentication initial setup

---
+++ services/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyBdJSSsIDI2xfAhdWhcIjjzrCAEFoXbo7E",
+  authDomain: "babitz.firebaseapp.com",
+  projectId: "babitz",
+  storageBucket: "babitz.appspot.com",
+  messagingSenderId: "500355572360",
+  appId: "1:500355572360:web:c04e9f2bf594ac9d79b53d",
+};
+
+export default function initFirebase() {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+  }
+  const auth = firebase.auth();
+}
