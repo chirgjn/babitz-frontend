957b89bd0fed02fd3d693be0728eb9f66b0a57b7
Siddharth Anand <45518363+siddanand@users.noreply.github.com>
2021-08-06 19:13:18 +0530

    feat: Add Restaurant Pages (#35)
    - Add checkout page.
    - Add login for end customer.
    - Integrated Razorpay for payment.
    - Add dynamic routes for restaurants for their custom pages.
    
    **References**
    
    - https://nextjs.org/docs/routing/dynamic-routes
    - https://razorpay.com/docs/payment-gateway/web-integration/standard/#step-1-create-an-order-from-your-server

---
+++ pages/[Restaurant]/checkout.js
@@ -0,0 +1,384 @@
+import Head from "next/head";
+import Script from "next/script";
+import Image from "next/image";
+import Link from "next/link";
+import withAuth from "../../helpers/withAuth";
+import firebase from "firebase/app";
+import { useEffect, useState } from "react";
+import Loader from "../../components/Loader";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+
+//----------Style
+var Heading = styled.p`
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 45px;
+  line-height: 40px;
+  color: #000000;
+`;
+const SubheadingDiv = styled.ul`
+  left: 100px;
+  margin-top: 50px;
+`;
+const Subheading = styled.li`
+  color: black;
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: lighter;
+  font-size: 25px;
+  list-style: "❑";
+  padding: 17px;
+  border-left: 2px solid grey;
+  @media (max-width: 768px) {
+    font-size: 20px;
+  }
+`;
+const Paymentbutton = styled.button`
+  margin: 10px 0px 0px 100px;
+  background: #afdfff;
+  border: none;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 25px;
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  color: #000000;
+  width: 300px;
+  @media (max-width: 768px) {
+    margin: 10px 0px 30px 50px;
+    width: auto;
+  }
+`;
+const Box = styled.div`
+  background: #f6f5f7;
+  box-shadow: 0px 0px 25px 5px rgba(0, 0, 0, 0.25);
+  border-radius: 25px;
+  padding: 20px 40px 20px 40px;
+  width: 90%;
+  float: right;
+  max-height: 500px;
+  overflow: auto;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const Itemhead = styled.p`
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 25px;
+  line-height: 40px;
+  color: #000000;
+`;
+
+const Itemname = styled.p`
+  margin-top: 0px;
+  font-family: "Sans-serif";
+  font-style: normal;
+  font-weight: 300;
+  font-size: 20px;
+  line-height: 40px;
+  color: #000000;
+`;
+const Backbutton = styled.button`
+  margin: 40px 0px 0px 40px;
+  background: #c4c4c4;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border: none;
+  border-radius: 25px;
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  color: #000000;
+  width: 150px;
+  @media (max-width: 768px) {
+    margin: 40px 0px 40px 0px;
+    width: auto;
+  }
+`;
+function Checkout() {
+  const router = useRouter();
+  const url = router.query;
+  const lower = url.Restaurant;
+  const restname = lower.charAt(0).toUpperCase() + lower.slice(1);
+  const [cartitems, setCartitems] = useState([]);
+  const [restid, setRestid] = useState("");
+  const [cartamount, setCartamount] = useState("");
+  const [useraddress, setUseraddress] = useState("");
+  const [userpincode, setUserpincode] = useState("");
+  const [userphone, setUserphone] = useState("");
+  const user = firebase.auth().currentUser;
+
+  useEffect(() => {
+    var requestOptions2 = {
+      redirect: "follow",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: user.Aa,
+      },
+    };
+    fetch(
+      "https://babitz-backend.herokuapp.com/getUserProfile",
+      requestOptions2
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        if (json.location != null) {
+          setUseraddress(json.location.firstLine);
+          setUserpincode(json.location.pincode);
+        }
+        setUserphone(json.mobileNumber);
+      });
+    var requestOptions = {
+      redirect: "follow",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    };
+    fetch(
+      "https://babitz-backend.herokuapp.com/getRestaurantByName?restautantName=" +
+        restname,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        var requestOptions1 = {
+          redirect: "follow",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: user.Aa,
+          },
+        };
+        fetch(
+          "https://babitz-backend.herokuapp.com/getCart/?restaurantId=" +
+            json.id,
+          requestOptions1
+        )
+          .then((response) => response.json())
+          .then((json) => {
+            setRestid(json.restaurantId);
+            setCartitems(json.items);
+            setCartamount(json.amount);
+          });
+      });
+  }, [restname, user]);
+
+  const back = () => {
+    if (restname != undefined) {
+      window.location = `/${restname}/main`;
+    }
+  };
+  const payment = (e) => {
+    e.preventDefault();
+    var formdata = {
+      location: {
+        firstLine: useraddress,
+        pincode: userpincode,
+      },
+      mobileNumber: userphone,
+    };
+    var requestOptions = {
+      method: "PATCH",
+      body: JSON.stringify(formdata),
+      redirect: "follow",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: user.Aa,
+      },
+    };
+    fetch(
+      "https://babitz-backend.herokuapp.com/updateUserDetails",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        var requestOptions1 = {
+          redirect: "follow",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: user.Aa,
+          },
+        };
+        fetch(
+          "https://babitz-backend.herokuapp.com/placeOrder/?restaurantId=" +
+            restid,
+          requestOptions1
+        )
+          .then((response) => response.json())
+          .then((json) => {
+            console.log(json);
+            let options = {
+              key: "rzp_test_lkIRdzaooxSzYo", // Enter the Key ID generated from the Dashboard
+              amount: cartamount * 100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+              currency: "INR",
+              name: "Babitz",
+              description: "Test Transaction",
+              // image: "https://example.com/your_logo",
+              order_id: json.paymentId, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+              handler: function (response) {},
+              prefill: {
+                name: user.displayName,
+                email: user.email,
+                contact: userphone,
+              },
+              notes: {
+                address: "Razorpay Corporate Office",
+              },
+              theme: {
+                color: "#3399cc",
+              },
+            };
+            const rzp1 = new window.Razorpay(options);
+
+            rzp1.open();
+          });
+      });
+  };
+  console.log(cartamount);
+  console.log(cartitems);
+
+  return (
+    <div>
+      <Head>
+        <script
+          async
+          src="https://checkout.razorpay.com/v1/checkout.js"
+        ></script>
+      </Head>
+      <div className="container">
+        <div id="react-root"></div>
+        <div className="row" style={{ marginTop: "40px" }}>
+          <div className="col-sm-6">
+            <Heading>Checkout</Heading>
+            <form>
+              <SubheadingDiv>
+                <Subheading>
+                  {" "}
+                  Delivery Information
+                  {/* -------------Do Address Validation ------------ */}
+                  <textarea
+                    defaultValue={useraddress}
+                    className="form-control"
+                    onChange={(e) => setUseraddress(e.target.value)}
+                    style={{
+                      boxShadow: "inset 0px 4px 4px rgba(0, 0, 0, 0.25)",
+                      borderRadius: "25px",
+                      padding: "20px",
+                      marginTop: "30px",
+                    }}
+                    placeholder="Enter Delivery Address"
+                    rows="4"
+                    required
+                  />
+                  <input
+                    defaultValue={userpincode}
+                    type="tel"
+                    onChange={(e) => setUserpincode(e.target.value)}
+                    className="form-control"
+                    pattern="^[1-9][0-9]{5}$"
+                    style={{
+                      boxShadow: "inset 0px 4px 4px rgba(0, 0, 0, 0.25)",
+                      borderRadius: "25px",
+                      padding: "20px",
+                      marginTop: "20px",
+                    }}
+                    placeholder="Pincode"
+                    required
+                  />
+                  <input
+                    type="tel"
+                    defaultValue={userphone}
+                    onChange={(e) => setUserphone(e.target.value)}
+                    className="form-control"
+                    pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                    style={{
+                      boxShadow: "inset 0px 4px 4px rgba(0, 0, 0, 0.25)",
+                      borderRadius: "25px",
+                      padding: "20px",
+                      marginTop: "20px",
+                    }}
+                    placeholder="Enter Mobile Number"
+                    required
+                  />
+                </Subheading>
+                <Subheading> Payment</Subheading>
+                <Paymentbutton
+                  onClick={payment}
+                  type="submit"
+                  className="btn btn-primary"
+                  style={{ outline: "none" }}
+                >
+                  Proceed to Payment
+                </Paymentbutton>
+              </SubheadingDiv>
+            </form>
+          </div>
+          <div className="col-sm-6">
+            <Box>
+              <center>
+                <Heading>Cart</Heading>
+              </center>
+              <div className="row" style={{ marginTop: "40px" }}>
+                <div className="col-xs-5">
+                  <Itemhead>Items</Itemhead>
+                </div>
+                <div className="col-xs-4">
+                  <Itemhead>Quantity</Itemhead>
+                </div>
+                <div className="col-xs-3">
+                  <Itemhead>Price</Itemhead>
+                </div>
+              </div>
+              {cartitems.map((data) => {
+                return (
+                  <div className="row" key={data.item.id}>
+                    <div className="col-xs-5">
+                      <Itemname>{data.item.name}</Itemname>
+                    </div>
+                    <div className="col-xs-4">
+                      <Itemname>{data.qty}</Itemname>
+                    </div>
+                    <div className="col-xs-3">
+                      <Itemname>₹ {data.item.price}</Itemname>
+                    </div>
+                  </div>
+                );
+              })}
+              <div className="row" style={{ borderTop: "1px solid lightgrey" }}>
+                <div className="col-xs-9">
+                  <Itemname>Total</Itemname>
+                </div>
+                <div className="col-xs-3">
+                  <Itemname>₹ {cartamount}</Itemname>
+                </div>
+              </div>
+            </Box>
+            <center>
+              <Backbutton
+                onClick={back}
+                type="button"
+                className="btn btn-default"
+                style={{ outline: "none" }}
+              >
+                Back to Menu
+              </Backbutton>
+            </center>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default withAuth(Checkout);
---
+++ pages/[Restaurant]/main.js
@@ -0,0 +1,417 @@
+import { useRouter } from "next/router";
+import withAuth from "../../helpers/withAuth";
+import firebase from "firebase/app";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Loader from "../../components/Loader";
+import styled from "styled-components";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+
+// -----------style
+const Logo = styled.p`
+  font-size: 37px;
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 67px;
+  color: lightgrey;
+`;
+const Home = styled.div`
+  min-height: 60vh;
+  width: 100%;
+  background-repeat: no-repeat;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  background-position: center;
+`;
+const Restname = styled.p`
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 80px;
+  line-height: 100px;
+  text-align: center;
+  letter-spacing: -0.015em;
+  color: white;
+`;
+const Restdescr = styled.p`
+  margin-top: 30px;
+  margin-bottom: 30px;
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 40px;
+  text-align: center;
+  color: white;
+`;
+const Items = styled.div`
+  background: #e5e5e5;
+  padding: 20px 0px 20px 0px;
+`;
+const Box = styled.div`
+  margin-top: 30px;
+  background: #ffffff;
+  border-radius: 25px;
+  padding: 20px 30px 20px 30px;
+`;
+const Itemname = styled.p`
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 30px;
+  line-height: 30px;
+`;
+const Itemprice = styled.p`
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 25px;
+  line-height: 30px;
+  letter-spacing: -0.015em;
+  color: #7d8491;
+`;
+const Itemdescr = styled.p`
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 20px;
+  line-height: 32px;
+  color: #7d8491;
+`;
+const Button = styled.button`
+  margin-top: 30px;
+  background: #c4c4c4;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border: none;
+  border-radius: 25px;
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 22px;
+  line-height: 33px;
+  text-align: center;
+  color: #000000;
+  padding: 0px 15px 0px 15px;
+`;
+const Footer = styled.div`
+  background: #c4c4c4;
+  padding: 40px 0px 40px 0px;
+`;
+const Location = styled.p`
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 40px;
+  line-height: 20px;
+  text-align: center;
+  color: #000000;
+`;
+const Address = styled.p`
+  font-family: Oswald;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 25px;
+  line-height: 50px;
+  text-align: center;
+  color: #000000;
+  margin-top: 20px;
+`;
+function Usermain() {
+  const router = useRouter();
+  const url = router.query;
+  const lower = url.Restaurant;
+  const restname = lower.charAt(0).toUpperCase() + lower.slice(1);
+  const user = firebase.auth().currentUser;
+
+  const [rest, setRest] = useState("");
+  const [restitems, setRestitems] = useState([]);
+  const [restid, setRestid] = useState("");
+  const [selectitems, setselectitems] = useState([
+    {
+      count: 0,
+      item: {},
+    },
+  ]);
+  const [cart, setCart] = useState("");
+  const [cartamount, setCartamount] = useState(0);
+
+  const myLoader = ({ src }) => {
+    return `https://babitz-s3.s3.ap-south-1.amazonaws.com/root/${restid}/${src}`;
+  };
+  const [selectitemscount, setselectitemscount] = useState(0);
+
+  useEffect(() => {
+    var requestOptions = {
+      redirect: "follow",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    };
+    fetch(
+      "https://babitz-backend.herokuapp.com/getRestaurantByName?restautantName=" +
+        restname,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setRest(json);
+        setRestid(json.id);
+        var requestOptions1 = {
+          redirect: "follow",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: user.Aa,
+          },
+        };
+        fetch(
+          "https://babitz-backend.herokuapp.com/getCart/?restaurantId=" +
+            json.id,
+          requestOptions1
+        )
+          .then((response) => response.json())
+          .then((json) => {
+            if (json) {
+              setCart(json);
+              setCartamount(json.amount);
+              setselectitems(json.items);
+            }
+          });
+      });
+    fetch(
+      "https://babitz-backend.herokuapp.com/getItemsByRestaurantName?restautantName" +
+        restname,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setRestitems(json);
+      });
+  }, [restname, user]);
+  function signOut() {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {})
+      .catch((error) => {});
+  }
+  const checkout = () => {
+    if (restname != undefined) {
+      var formdata = cart;
+      formdata.amount = cartamount;
+      formdata.items = selectitems;
+      var requestOptions = {
+        method: "PATCH",
+        body: JSON.stringify(formdata),
+        redirect: "follow",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: user.Aa,
+        },
+      };
+      fetch(
+        "https://babitz-backend.herokuapp.com/updateWholeCart",
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          window.location = `/${restname}/checkout`;
+        });
+    }
+  };
+  const decrement_count = (item) => {
+    let id = item.id;
+    let index = selectitems.findIndex((x) => x.item.id === id);
+    try {
+      if (selectitems[index].qty > 1) {
+        let temporaryarray = selectitems.slice();
+        temporaryarray[index].qty -= 1;
+        setselectitems(temporaryarray);
+      } else {
+        let temporaryarray = selectitems;
+        temporaryarray.splice(index, 1);
+      }
+      let amount = cartamount;
+      amount = amount - item.price;
+      setCartamount(amount);
+    } catch {}
+  };
+  const increment_count = (item) => {
+    let id = item.id;
+    let index = selectitems.findIndex((x) => x.item.id === id);
+    try {
+      if (selectitems[index].qty) {
+        let temporaryarray = selectitems.slice();
+        temporaryarray[index].qty += 1;
+        setselectitems(temporaryarray);
+      }
+    } catch {
+      let obj = {};
+      obj["item"] = item;
+      obj["qty"] = 1;
+      var arr = selectitems;
+      arr.push(obj);
+      setselectitems(arr);
+    }
+    let amount = cartamount;
+    amount = amount + item.price;
+    setCartamount(amount);
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>{rest.name}</title>
+        <meta name="description" content="Generated by create next app" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"
+        />
+        <script
+          async
+          src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+        ></script>
+        <script
+          async
+          src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
+        ></script>
+      </Head>
+      <Home
+        style={{
+          backgroundImage: `url("https://babitz-s3.s3.ap-south-1.amazonaws.com/root/${restid}/banner")`,
+        }}
+      >
+        <div className="container">
+          <div>
+            <Image
+              style={{ display: "inline-block" }}
+              loader={myLoader}
+              width={100}
+              height={100}
+              src="logo"
+              alt=""
+            />
+            <div
+              style={{
+                float: "right",
+                display: "inline-block",
+                marginTop: "20px",
+              }}
+            >
+              <span
+                onClick={checkout}
+                style={{ color: "white", fontSize: "20px", cursor: "pointer" }}
+                className="glyphicon glyphicon-shopping-cart"
+              ></span>
+              <span
+                onClick={signOut}
+                style={{
+                  color: "white",
+                  marginLeft: "30px",
+                  fontSize: "20px",
+                  cursor: "pointer",
+                }}
+                className="glyphicon glyphicon-log-out"
+              ></span>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-6">
+              <Restname>{rest.name || <Skeleton />}</Restname>
+              <Restdescr>{rest.description || <Skeleton />}</Restdescr>
+            </div>
+          </div>
+        </div>
+      </Home>
+      <Items>
+        <div className="container" style={{ height: "70vh", overflow: "auto" }}>
+          <div className="row">
+            <div className="col-sm-1"></div>
+            <div className="col-sm-10">
+              {restitems.map((item) => {
+                return (
+                  <div key={item.id}>
+                    <Box>
+                      <div className="row">
+                        <div className="col-sm-8">
+                          <Itemname>{item.name || <Skeleton />}</Itemname>
+                          <Itemprice>₹ {item.price || <Skeleton />}</Itemprice>
+                          <Itemdescr>
+                            {item.description || <Skeleton />}{" "}
+                          </Itemdescr>
+                          <span></span>
+                          <span>
+                            <div
+                              style={{
+                                padding: "10px",
+                                background: "lightgrey",
+                                width: "100px",
+                                textAlign: "center",
+                                borderRadius: "10px",
+                                fontSize: "15px",
+                                fontFamily: "Oswald",
+                              }}
+                            >
+                              <span
+                                onClick={() => decrement_count(item)}
+                                style={{
+                                  marginRight: "10px",
+                                  cursor: "pointer",
+                                }}
+                                className="glyphicon glyphicon-minus"
+                              ></span>
+                              {selectitems.map((data) => {
+                                if (data.item.id == item.id) {
+                                  return (
+                                    <span key={data.item.id}>{data.qty}</span>
+                                  );
+                                }
+                              })}
+                              <span
+                                onClick={() => increment_count(item)}
+                                style={{
+                                  marginLeft: "10px",
+                                  cursor: "pointer",
+                                }}
+                                className="glyphicon glyphicon-plus"
+                              ></span>
+                            </div>
+                          </span>
+                          <span></span>
+                        </div>
+                        <div className="col-sm-4">
+                          <div style={{ float: "right" }}>
+                            <Image
+                              src="/Image_Item1.png"
+                              layout="intrinsic"
+                              width="150px"
+                              height="150px"
+                              alt=""
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </Box>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="col-sm-1"></div>
+          </div>
+        </div>
+      </Items>
+      <Footer>
+        <Location>Location</Location>
+        <Address>{rest.address}</Address>
+      </Footer>
+    </div>
+  );
+}
+export default withAuth(Usermain);
